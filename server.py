b1e4ad294907df2a8421c8fd8032fdd0c2d87a4e
devashish.shankar <devashish.shankar@flipkart.com>
2015-07-28 14:13:07 +0530

    changing to ascii encoding

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = 'F34TF$($e34D';   #Required for flask server TODO che
 @app.route('/')
 def home():
     #The tweet to classify
-    tweet=str(request.args['text']).encode('utf-8').lower()
+    tweet=str(request.args['text']).encode('ascii', 'ignore').lower()
     #The path to file containing the model
     model=str(request.args['model'])
     #Should the tweet be preprocessed
